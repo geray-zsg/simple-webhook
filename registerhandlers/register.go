02e4394ec59658bbc2fea382372454cfa18f9458
Geray <1690014753@qq.com>
2024-06-19 17:47:58 +0800

    修改了handler注册命名和Admission webhook命名规则

--- registerhandlers/register.go
+++ registerhandlers/register.go
@@ -9,15 +9,15 @@ import (
 // func RegisterHandlers(configEnable types.ConfigEnabel, labels []string) {
 func RegisterHandlers(configEnable types.ConfigEnabel, configHandlersParameters types.ConfigHandlersParameters) {
 	if configEnable.MutatePodEnvInjectedHandle {
-		http.HandleFunc("/mutate", handlers.PodEnvInjectedHandleMutate)
+		http.HandleFunc("/mutatePod", handlers.PodEnvInjectedHandleMutate)
 	}
 	if configEnable.ValidateNamespaceLabelsHandle {
 		// glog.Info("configHandlersParameters.LabelsToCheck:", labels)
 		// glog.Info("configHandlersParameters.LabelsToCheck:", configHandlersParameters.LabelsToCheck)
-		http.HandleFunc("/validate", handlers.NamespaceLabelsHandleValidate(configHandlersParameters.LabelsToCheck))
+		http.HandleFunc("/validateNamespace", handlers.NamespaceLabelsHandleValidate(configHandlersParameters.LabelsToCheck))
 		// http.HandleFunc("/validate", handlers.NamespaceLabelsHandleValidate(configHandlersParameters.LabelsToCheck))
 	} else {
-		http.HandleFunc("/validate", handlers.AllowedHandlers())
+		http.HandleFunc("/validateNamespace", handlers.AllowedHandlers())
 	}
 
 	if configEnable.ValidateCheckDeploymentPrefix {
