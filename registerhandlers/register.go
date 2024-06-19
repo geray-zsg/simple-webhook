007fb78bb8216a815a72f4280ecc45789e96d2dc
Geray <1690014753@qq.com>
2024-06-19 18:02:09 +0800

    admission webhook 路径不支持大小改为小写和连接符

--- registerhandlers/register.go
+++ registerhandlers/register.go
@@ -9,20 +9,20 @@ import (
 // func RegisterHandlers(configEnable types.ConfigEnabel, labels []string) {
 func RegisterHandlers(configEnable types.ConfigEnabel, configHandlersParameters types.ConfigHandlersParameters) {
 	if configEnable.MutatePodEnvInjectedHandle {
-		http.HandleFunc("/mutatePod", handlers.PodEnvInjectedHandleMutate)
+		http.HandleFunc("/mutate-pod", handlers.PodEnvInjectedHandleMutate)
 	}
 	if configEnable.ValidateNamespaceLabelsHandle {
 		// glog.Info("configHandlersParameters.LabelsToCheck:", labels)
 		// glog.Info("configHandlersParameters.LabelsToCheck:", configHandlersParameters.LabelsToCheck)
-		http.HandleFunc("/validateNamespace", handlers.NamespaceLabelsHandleValidate(configHandlersParameters.LabelsToCheck))
+		http.HandleFunc("/validate-namespace", handlers.NamespaceLabelsHandleValidate(configHandlersParameters.LabelsToCheck))
 		// http.HandleFunc("/validate", handlers.NamespaceLabelsHandleValidate(configHandlersParameters.LabelsToCheck))
 	} else {
-		http.HandleFunc("/validateNamespace", handlers.AllowedHandlers())
+		http.HandleFunc("/validate-namespace", handlers.AllowedHandlers())
 	}
 
 	if configEnable.ValidateCheckDeploymentPrefix {
-		http.HandleFunc("/validateDeploy", handlers.CheckDeployPrefixHandleValidate(configHandlersParameters.DeploymentPrefix))
+		http.HandleFunc("/validate-deploy", handlers.CheckDeployPrefixHandleValidate(configHandlersParameters.DeploymentPrefix))
 	} else {
-		http.HandleFunc("/validateDeploy", handlers.AllowedHandlers())
+		http.HandleFunc("/validate-deploy", handlers.AllowedHandlers())
 	}
 }
